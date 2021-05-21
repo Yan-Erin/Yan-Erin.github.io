3d2cc44c44b661f968cc037cee35a9fa18978adf
glowyRectangle <68751020+glowyRectangle@users.noreply.github.com>
2021-05-21 16:54:39 -0400

    whiteboard

---
+++ docs/whiteboard.js
@@ -0,0 +1,56 @@
+var colour = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+// When clicking on colours items
+$(".controls").on("click", "li", function () {
+    //  Deselect aibling elements
+    $(this).siblings().removeClass("selected");
+    //  Select clicked element
+    $(this).addClass("selected");
+
+    // Cache current colour
+    colour = $(this).css("background-color");
+
+});
+
+
+// When New colour is pressed by user
+$("#revealColorSelect").click(function () {
+    // Show colour select or hide the color select
+    changeColor();
+    $("#colorSelect").toggle();
+});
+
+// Update the new colour span
+function changeColor() {
+    var r = $("#red").val();
+    var g = $("#green").val();
+    var b = $("#blue").val();
+    $("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+}
+
+
+// On mouse events on the canvas
+$canvas.mousedown(function (e) {
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function (e) {
+    // Draw lines
+    if (mouseDown) {
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = colour;
+        context.lineWidth = 5;
+        context.lineCap = 'round';
+        context.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function () {
+    mouseDown = false;
+}).mouseleave(function () {
+    $canvas.mouseup();
+});
